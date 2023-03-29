a7dcac0b799699b0f3ab2d706b0fe5f6eec83025
xeniakadar <xenia.kadar@gmail.com>
2023-03-29 19:19:51 +0200

    added general component

--- cv-project/src/App.js
+++ cv-project/src/App.js
@@ -6,12 +6,16 @@ import Education from './components/Education'
 import Experience from './components/Experience'
 
 class App extends Component {
+  constructor() {
+    super();
+
+  }
+
   render() {
     return (
       <div>
         <General />
-        <Education />
-        <Experience />
+
 
       </div>
   )
--- cv-project/src/components/General.js
+++ cv-project/src/components/General.js
@@ -1,10 +1,83 @@
-import React from 'react';
 
-const General = (props) => {
-  return (
-    <h1>General</h1>
-  )
+import React, {Component} from 'react';
+
+class General extends Component{
+  state = {
+    name: '',
+    email: '',
+    phone: '',
+    isEditing: true
+  };
+
+
+  handleChange = (event) => {
+    const { name, value } = event.target;
+    this.setState({
+      [name]: value,
+    });
+  }
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+    this.setState({
+     isEditing: false,
+    })
+  }
+
+  handleEdit = () => {
+    this.setState({
+      isEditing: true
+    })
+  }
+
+  render() {
+    const {fullName, email, phone, isEditing} = this.state
+    return (
+      <div>
+        {isEditing ? (
+          <div>
+            <h1>Contact</h1>
+            <form>
+              <label htmlFor='fullNameInput'>First Name</label>
+              <input
+                type='text'
+                id="fullNameInput"
+                name="fullName"
+                value={fullName}
+                onChange={this.handleChange} />
+
+              <label htmlFor='emailInput'>Email</label>
+              <input
+                type='text'
+                name="email"
+                id="emailInput"
+                value={email}
+                onChange={this.handleChange} />
+
+              <label htmlFor='phoneInput'>Phone Number</label>
+              <input
+                type='text'
+                id="phoneInput"
+                name="phone"
+                value={phone}
+                onChange={this.handleChange} />
+            </form>
+            <button onClick={this.handleSubmit}>Submit</button>
+          </div>
+        ) : (
+          <div>
+              <h1>Name: {fullName}</h1>
+              <h1>Email: {email}</h1>
+              <h1>Phone: {phone}</h1>
+              <button onClick={this.handleEdit}>Edit</button>
+            </div>
+        )}
+
+      </div>
+    )
+  }
 }
+
 
 
 export default General;
